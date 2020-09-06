c6326b74d19ff108531fd4a58e0a8826102d0b45
Alysson Alves <alyssonja23@gmail.com>
2020-09-06 20:01:40 -0300

    Created Project

---
+++ src/test/java/peneira/start/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package peneira.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
