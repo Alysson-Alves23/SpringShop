1d6152c59c8937cd57ba0e143fb9769fa0e80b85
Alysson Alves <alyssonja23@gmail.com>
2020-09-06 22:04:01 -0300

    Domains Test

---
+++ src/main/java/curso/start/domain/Categorie.java
@@ -0,0 +1,50 @@
+package curso.start.domain;
+
+import java.io.Serializable;
+
+public class Categorie implements Serializable{
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name;
+	
+	public Categorie(int id, String name) {
+		setId(id);
+		setName(name);
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String nome) {
+		this.name = nome;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categorie other = (Categorie) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/curso/start/resource/CategoriesResource.java
+++ src/main/java/curso/start/resource/CategoriesResource.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import curso.start.domain.Categorie;
+
 @RestController 
-@RequestMapping(value= "/project_list") 
-public class CategoriaResource {
+@RequestMapping(value= "/categories") 
+public class CategoriesResource {
 	
 	@GetMapping
 	public HttpEntity<List> listar() {
 		
-		List projetos= new ArrayList();
-		projetos.add("Aula 14");
+		List<Categorie> projetos= new ArrayList();
+		Categorie Informatica= new Categorie(1,"Informática");
+		Categorie Mecanica= new Categorie(2,"Mecânica");
+		projetos.add(Informatica);
+		projetos.add(Mecanica);
 		return new HttpEntity<List>(projetos);
 	}
 }
